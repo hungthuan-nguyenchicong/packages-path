4df7ab87bcbffc52e49c7c05e0dec2cd1ef51234
nguyenchicong <nguyenchicong1502@gmail.com>
2026-02-04 19:23:36 +0700

    up bash setup

---
+++ setup/bash/copy-composer.sh
@@ -0,0 +1,20 @@
+#!bin/bash
+# branch
+CURRENT_BRANCH=$(cd "$ROOT_PATH" && git rev-parse --abbrev-ref HEAD)
+echo "Đang ở nhánh: $CURRENT_BRANCH. Tự động cấu hình Composer..."
+# sed -i "s#dev-*#dev-$CURRENT_BRANCH#g" "$DEST_FILE"
+
+SOURCE_FILE="$ROOT_PATH/setup/laravel-app/composer.json"
+TARGET_FILE="$ROOT_LARAVEL_APP/laravel-app/composer.json"
+
+# copy composer.json
+cp "$SOURCE_FILE" "$TARGET_FILE"
+
+# run
+(
+    
+    # cd laravel-app
+    cd "$RUN_LARAVEL" || exit
+    composer update vendor-path/packages-src --prefer-source
+)
+echo "Copy:: $SOURCE_FILE -> $TARGET_FILE"
---
+++ setup/setup.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+
+SCRIPT_DIR=$(dirname -- $BASH_SOURCE)
+
+# setup/setup.sh
+export ROOT_PATH=$(cd -- "$SCRIPT_DIR/.." && pwd)
+# $HOME/git/packages-app/laravel-app
+export ROOT_LARAVEL_APP=$(cd -- "$HOME/git/packages-app" && pwd)
+# cd run laravel
+export RUN_LARAVEL=$(cd -- "$ROOT_LARAVEL_APP/laravel-app" && pwd)
+# run bash
+bash "$ROOT_PATH/setup/bash/copy-composer.sh"
+
+
+echo "ROOT_PATH: $ROOT_PATH"
+echo "ROOT_LARAVEL_APP: $ROOT_LARAVEL_APP"
